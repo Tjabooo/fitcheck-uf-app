c6410c0f287fe2d40f788572f8e4c81b20d9b67d
Tjabooo <frollegaming@hotmail.com>
2024-10-10 15:51:18 +0200

    Added modular desktop restriction + fixed mobile UI

--- includes/functions.php
+++ includes/functions.php
@@ -10,31 +10,33 @@ function render_template($template) {
 
 // Block desktop access
 function blockDesktopAccess() {
-    if (!preg_match('/Mobi|Android|iPhone|iPad/i', $_SERVER['HTTP_USER_AGENT'])) {
-        echo '<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <title>Access Restricted</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link href="https://fonts.googleapis.com/css?family=Outfit" rel="stylesheet">
-            <style>
-                body {
-                    text-align: center;
-                    padding: 50px;
-                    font-family: "Outfit", sans-serif;
-                }
-                h1 {
-                    color: red;
-                }
-            </style>
-        </head>
-        <body>
-            <h1>Access Restricted</h1>
-            <p>This site is only accessible on mobile devices. Please visit using a mobile device.</p>
-        </body>
-        </html>';
-        exit;
+    if (0) { // 0 = disable, 1 = enable
+        if (!preg_match('/Mobi|Android|iPhone|iPad/i', $_SERVER['HTTP_USER_AGENT'])) {
+            echo '<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Access Restricted</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://fonts.googleapis.com/css?family=Outfit" rel="stylesheet">
+                <style>
+                    body {
+                        text-align: center;
+                        padding: 50px;
+                        font-family: "Outfit", sans-serif;
+                    }
+                    h1 {
+                        color: red;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>Access Restricted</h1>
+                <p>This site is only accessible on mobile devices. Please visit using a mobile device.</p>
+            </body>
+            </html>';
+            exit;
+        }
     }
 }
 
