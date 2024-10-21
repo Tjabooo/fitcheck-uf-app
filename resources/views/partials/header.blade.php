f213bd10e724bbc178e147d6272b46295fcfbfac
Tjabooo <frollegaming@hotmail.com>
2024-10-21 22:35:30 +0200

    Added header-left to password reset routes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,12 @@
 <header>
     <div class="header">
+        @if (request()->is('password/reset') || request()->is('password/email'))
+            <div class="header-left">
+                <button class="main-button-design" onclick=location.href="{{ url('/login') }}">
+                    Tillbaks till inloggning
+                </button>
+            </div>
+        @endif
         <div class="header-center">
             <img src="{{ asset('assets/logo.png') }}" alt="Logo"/>
         </div>
