486a355913dca353facdc66463b4115483c37d10
Tjabooo <frollegaming@hotmail.com>
2024-11-12 21:14:25 +0100

    updated add-clothing screen

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -79,8 +79,8 @@ function startCamera() {
         navigator.mediaDevices.getUserMedia({
             video: {
                 facingMode: "environment",
-                width: { ideal: 720 },
-                height: { ideal: 1280 }
+                width: { ideal: 1280 },
+                height: { ideal: 720 }
             }
         })
             .then(stream => {
