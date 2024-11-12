64541ac7290bb2624c33dcaac79cd34babc9660e
Tjabooo <frollegaming@hotmail.com>
2024-11-12 09:36:19 +0100

    blocked direct indexing to images? perhaps? perchance?

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,38 +3,38 @@
 @section('title', 'Logga in - FitCheck UF')
 
 @section('content')
-<div class="auth-container">
-    <h2>Logga in</h2>
-    <form action="{{ route('login') }}" method="post">
-        @csrf
-        @if (session('status'))
-            <div class="success-message">
-                {{ session('status') }}
+    <div class="auth-container">
+        <h2>Logga in</h2>
+        <form action="{{ route('login') }}" method="post">
+            @csrf
+            @if (session('status'))
+                <div class="success-message">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <div class="form-group @error('email') has-error @enderror">
+                <input type="email" name="email" placeholder="E-post" value="{{ old('email') }}" required>
+                @error('email')
+                    <span class="help-block">{{ $message }}</span>
+                @enderror
             </div>
-        @endif
-        <div class="form-group @error('email') has-error @enderror">
-            <input type="email" name="email" placeholder="E-post" value="{{ old('email') }}" required>
-            @error('email')
-                <span class="help-block">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="form-group @error('password') has-error @enderror">
-            <input type="password" name="password" placeholder="Lösenord" required>
-            @error('password')
-                <span class="help-block">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="form-group">
-            <label>
-            <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-            Kom ihåg mig
-            </label>
-        </div>
-        <div class="form-group">
-            <button class="main-button-design" type="submit">Logga in</button>
-        </div>
-        <p><a href="{{ route('password.request') }}">Glömt lösenord?</a></p>
-        <p>Har du inget konto? <a href="{{ route('register.index') }}">Registrera dig här</a>.</p>
-    </form>
-</div>
+            <div class="form-group @error('password') has-error @enderror">
+                <input type="password" name="password" placeholder="Lösenord" required>
+                @error('password')
+                    <span class="help-block">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="form-group checkbox-group">
+                <label>
+                    <input class="check" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <p class="check-label">Kom ihåg mig</p>
+                </label>
+            </div>
+            <div class="form-group">
+                <button class="main-button-design" type="submit">Logga in</button>
+            </div>
+            <p><a href="{{ route('password.request') }}">Glömt lösenord?</a></p>
+            <p>Har du inget konto? <a href="{{ route('register.index') }}">Registrera dig här</a>.</p>
+        </form>
+    </div>
 @endsection
